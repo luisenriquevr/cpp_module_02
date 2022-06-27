aae288943ee8ca4d3845ab00a705f64ddc43953f
luisenriquevr <trabajo.luisen@gmail.com>
2022-06-27 15:31:52 +0200

    ex00 initialized

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvarela <lvarela@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/27 15:28:51 by lvarela           #+#    #+#             */
+/*   Updated: 2022/06/27 15:31:19 by lvarela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class	Fixed {
+	private:
+		int					_value;
+		static const int	_fractional_bits;
+	public:
+		
+}
