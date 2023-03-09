38f63a077a1e416a2d17abcb1d6260e314af2b9f
Luis Enrique Varela <74375023+luisenriquevr@users.noreply.github.com>
2023-03-09 20:45:10 +0100

    Update main.cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,25 +1,2 @@
 #include "Fixed.hpp"
 #include "Point.hpp"
-
-int main( void ) {
-
-    Point const a(1, 1);
-    Point const b(6, 1);
-    Point const c(3, 4);
-    Point const point(3.25, 2.8764);    //inside
-    Point const point2(7.0005, 2.8764); //outside
-    Point const point3(3, 4);           //outside
-
-    bool result = bsp(a, b, c, point);
-    std::cout << "The point is in the triangle: " << result << std::endl;
-    result = bsp(a, b, c, point2);
-    std::cout << "The point is in the triangle: " << result << std::endl;
-    result = bsp(a, b, c, point3);
-    std::cout << "The point is in the triangle: " << result << std::endl;
-
-    result = bsp( Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 10) ); //inside
-    std::cout << "The point is in the triangle: " << result << std::endl;
-    
-    return 0;
-    
-}
